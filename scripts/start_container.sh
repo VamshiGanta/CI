b5bff30235cb0c2751f8fded8c3db88085286a3c
VamshiGanta <139753956+VamshiGanta@users.noreply.github.com>
2023-07-24 17:08:17 +0200

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull vamshiganta/pipleline-flask-app:tagname
+docker pull vamshiganta/pipleline-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 vamshiganta/pipleline-flask-app:tagname
+docker run -d -p 5000:5000 vamshiganta/pipleline-flask-app:latest
