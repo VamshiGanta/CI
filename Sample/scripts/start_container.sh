0381c7f35d45fbc453da3e1012a35eb8d0457f60
VamshiGanta <139753956+VamshiGanta@users.noreply.github.com>
2023-07-24 14:04:45 +0200

    space

--- Sample/scripts/start_container.sh
+++ Sample/scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull vamshiganta/pipleline-flask-app:tagname
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 vamshiganta/pipleline-flask-app:tagname
