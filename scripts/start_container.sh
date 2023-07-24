e6b04310f53aa6a457f031af329c79f891274d7e
VamshiGanta <139753956+VamshiGanta@users.noreply.github.com>
2023-07-24 17:18:51 +0200

    final confloct

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,3 +6,5 @@ docker pull vamshiganta/pipleline-flask-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 vamshiganta/pipleline-flask-app:latest
+
+docker run -d -p 5000:5000 vamshiganta/pipleline-flask-app:latest
